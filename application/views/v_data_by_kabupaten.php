173501503b98024f4750c93db527af19ce879b1e
ekza97 <ekza97@gmail.com>
2021-10-31 03:40:48 +0900

    Update v_data_by_kabupaten

--- application/views/v_data_by_kabupaten.php
+++ application/views/v_data_by_kabupaten.php
@@ -19,7 +19,7 @@
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h3 class="float-left">
-                    <?= ucwords(namaKabupaten(decrypt_url($this->uri->segment(3)))); ?>
+                    Proposal Pendidikan <?= ucwords(namaKabupaten(decrypt_url($this->uri->segment(3)))); ?>
                 </h3>
                 <a href="<?=base_url('welcome');?>" class="btn btn-sm btn-secondary float-right"><span
                         class="fa fa-arrow-left"></span>
@@ -32,61 +32,112 @@
                         <thead>
                             <tr>
                                 <th width="25">NO</th>
-                                <th>JENIS DOK LINGKUNGAN</th>
-                                <th>JENIS USAHA/KEGIATAN</th>
-                                <th>PEMRAKARSA/PENANGGUNG JAWAB</th>
-                                <th>ALAMAT</th>
-                                <th>LUAS LAHAN</th>
-                                <th>TAHUN OPERASI</th>
-                                <th>NO.IZIN/PERLING/Rekom SPPL</th>
-                                <th>INSTANSI PENERBIT</th>
-                                <th>STATUS PELAPORAN</th>
-                                <th>FILE IZIN</th>
-                                <th>FILE LOKASI</th>
+                                <th>KAB.</th>
+                                <th>KATEGORI</th>
+                                <th>NAMA LENGKAP</th>
+                                <th>TTL</th>
+                                <th>INSTITUSI</th>
+                                <th>FAKULTAS</th>
+                                <th>JURUSAN</th>
+                                <th>PRODI</th>
+                                <th>STATUS</th>
+                                <th width="170">AKSI</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php $n=1; foreach($data_by_jenis_usaha as $row): ?>
+                            <?php $n=1; foreach($all_data as $row): ?>
                             <tr>
                                 <td><?= $n++; ?></td>
-                                <td><?= strtoupper($row['jenis_usaha']); ?></td>
-                                <td>
-                                    <a href="https://www.google.com/maps/place/<?=$row['latitude'].', '.$row['longitude'];?>"
-                                        target="_blank">
-                                        <?= ucfirst($row['nama_tempat']); ?>
-                                    </a>
-                                </td>
-                                <td><?= ucfirst($row['nama_pemilik']); ?></td>
-                                <td><?= ucfirst($row['alamat']); ?></td>
-                                <td><?= $row['luas_lahan'].' Ha'; ?></td>
-                                <td><?= $row['tahun_operasi']; ?></td>
-                                <td><?= strtoupper($row['nomor_izin']); ?></td>
-                                <td><?= strtoupper($row['instansi_penerbit_izin']); ?></td>
-                                <td>
-                                    <?php if($row['status_izin']=='Aktif'): ?>
-                                    <span class="badge badge-success"><?= $row['status_izin']; ?></span>
+                                <td><?= strtoupper($row['nama_kabupaten']); ?></td>
+                                <td><?= strtoupper($row['kode_kategori']); ?></td>
+                                <td><?= ucwords($row['nama_lengkap']); ?></td>
+                                <td><?= $row['tmp_lahir'].', '.date('d F Y',strtotime($row['tgl_lahir'])); ?></td>
+                                <td><?= ucwords($row['institusi']); ?></td>
+                                <td><?= ucwords($row['fakultas']); ?></td>
+                                <td><?= ucwords($row['jurusan']); ?></td>
+                                <td><?= ucwords($row['prodi']); ?></td>
+                                <td align="center">
+                                    <?php if($row['status']=='Baru'): ?>
+                                    <span class="badge badge-secondary"><?= $row['status']; ?></span>
+                                    <?php elseif($row['status']=='Verifikasi'): ?>
+                                    <span class="badge badge-primary"><?= $row['status']; ?></span>
+                                    <?php elseif($row['status']=='Lolos'): ?>
+                                    <span class="badge badge-success"><?= $row['status']; ?></span>
+                                    <?php elseif($row['status']=='Pencairan'): ?>
+                                    <span class="badge badge-warning"><?= $row['status']; ?></span>
                                     <?php else: ?>
-                                    <span class="badge badge-danger"><?= $row['status_izin']; ?></span>
+                                    <span class="badge badge-danger"><?= $row['status']; ?></span>
                                     <?php endif; ?>
                                 </td>
-                                <td>
-                                    <?php if($row['file_izin']==''): ?>
-                                    <span class="badge badge-danger">File Kosong</span>
-                                    <?php else: ?>
-                                    <a href="<?=base_url();?>uploads/<?=$row['file_izin'];?>" target="_blank"
+                                <td class="text-center">
+                                    <a href="<?=base_url('proposal/pberkas/').$row['idproposal_pendidikan'];?>"
                                         class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="top"
-                                        title="View/Download File"><i class="fa fa-eye"></i>
+                                        title="Unggah Berkas"><i class="fas fa-upload"></i>
                                     </a>
-                                    <?php endif; ?>
+                                    <a href="#" class="btn btn-info btn-sm" data-toggle="tooltip" data-placement="top"
+                                        title="Ubah Data" onclick="edit(<?=$row['idproposal_pendidikan'];?>)"><i
+                                            class="fas fa-edit"></i>
+                                    </a>
+                                    <a href="<?=base_url('proposal/hapus_pend/');?><?=$row['idproposal_pendidikan'];?>"
+                                        class="btn btn-danger btn-sm btn-hapus" data-toggle="tooltip"
+                                        data-placement="top" title="Hapus Data"><i class="fas fa-trash"></i> </a>
                                 </td>
-                                <td>
-                                    <?php if($row['file_lokasi']==''): ?>
-                                    <span class="badge badge-danger">File Kosong</span>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Causes Section -->
+<section class="causes-section style-two">
+    <div class="auto-container">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h3 class="float-left">
+                    Proposal Umum<?= ucwords(namaKabupaten(decrypt_url($this->uri->segment(3)))); ?>
+                </h3>
+                <a href="<?=base_url('welcome');?>" class="btn btn-sm btn-secondary float-right"><span
+                        class="fa fa-arrow-left"></span>
+                    Kembali</a>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover table-sm text-nowrap" id="dataTable" width="100%"
+                        cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th width="25">NO</th>
+                                <th>KAB.</th>
+                                <th>KATEGORI</th>
+                                <th>NAMA</th>
+                                <th>PEMOHON</th>
+                                <th>KEGIATAN</th>
+                                <th>STATUS</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $n=1; foreach($all_data as $row): ?>
+                            <tr>
+                                <td><?= $n++; ?></td>
+                                <td><?= strtoupper($row['nama_kabupaten']); ?></td>
+                                <td><?= strtoupper($row['kode_kategori']); ?></td>
+                                <td><?= ucwords($row['nama']); ?></td>
+                                <td><?= ucwords($row['pemohon']); ?></td>
+                                <td><?= ucfirst($row['kegiatan']); ?></td>
+                                <td align="center">
+                                    <?php if($row['status']=='Baru'): ?>
+                                    <span class="badge badge-secondary"><?= $row['status']; ?></span>
+                                    <?php elseif($row['status']=='Verifikasi'): ?>
+                                    <span class="badge badge-primary"><?= $row['status']; ?></span>
+                                    <?php elseif($row['status']=='Lolos'): ?>
+                                    <span class="badge badge-success"><?= $row['status']; ?></span>
+                                    <?php elseif($row['status']=='Pencairan'): ?>
+                                    <span class="badge badge-warning"><?= $row['status']; ?></span>
                                     <?php else: ?>
-                                    <a href="<?=base_url();?>uploads/<?=$row['file_lokasi'];?>" target="_blank"
-                                        class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="top"
-                                        title="View/Download File"><i class="fa fa-eye"></i>
-                                    </a>
+                                    <span class="badge badge-danger"><?= $row['status']; ?></span>
                                     <?php endif; ?>
                                 </td>
                             </tr>
